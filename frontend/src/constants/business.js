1d244e9f545c680d68066b192b37618f01f5acdf
PrafullJ001 <jprafull40@gmail.com>
2026-08-01 16:26:50 +0530

    updated messege templet

--- frontend/src/constants/business.js
+++ frontend/src/constants/business.js
@@ -1,3 +1,3 @@
-export const BUSINESS_NAME = "Shree Krishna ESS Spraying Services Pimpri";
+export const BUSINESS_NAME = "श्रीकृष्ण ESS सर्व्हिसेस, पिंपरी";
 export const BUSINESS_CONTACT = "+91 9423068234";
 export const BUSINESS_PHONEPE = "+91 9637972009";
